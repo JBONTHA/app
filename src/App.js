eb3ec5dc37bc6d59e347bd0e5f397fabc3c3bc09
J BONTHA <bonthajoseph4@gmail.com>
2023-12-19 11:16:45 +0530

    changed class file name

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import EmployeeComponent from './EmployeeComponent';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
+    <Router>
+    <div className="Application">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,7 +22,19 @@ function App() {
           Learn React
         </a>
       </header>
+      <div>
+      <h1>Employee Management App</h1>
+      <EmployeeComponent />
+      <Routes>
+            <Route path="/register" component={RegisterCompo} />
+            <Route path="/login" component={LoginComponent} />
+            <Route path="/update/:id" component={UpdateComponent} />
+            {/* Add other routes as needed */}
+            <Route path="/" component={EmployeeListComponent} />
+          </Routes>
     </div>
+    </div>
+   </Router> 
   );
 }
 
